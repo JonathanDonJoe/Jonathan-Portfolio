003a87212d098ea3827ee4588778a4f8dcf31f77
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-04 00:45:23 -0400

    Fixed skill icon responsiveness on load

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,24 @@
 let skillIcons = document.querySelectorAll('.skill-icon');
 console.log(skillIcons);
 
-window.onresize = function() {
+// window.onresize = function() {
+
+function resizeSkillIcons() { 
     // console.log('hi')
     if (window.innerWidth > 375) {
         skillIcons.forEach( (element) => {
             element.classList.remove('fa-3x');
             element.classList.add('fa-7x');
-            // console.log('hi')
+            console.log('big window')
         })
     } else {
-            skillIcons.forEach( (element) => {
-                element.classList.remove('fa-7x');
-                element.classList.add('fa-3x');
-            })
+        skillIcons.forEach( (element) => {
+            element.classList.remove('fa-7x');
+            element.classList.add('fa-3x');
+            console.log('small window')
+        })
     }
 }
+
+window.addEventListener("resize", resizeSkillIcons);
+window.onload = resizeSkillIcons;
