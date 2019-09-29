732495d121fbcc70fee0de8f7b411f2000d89ba8
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-29 00:06:29 -0400

    Added redux icon.  Fixed more responsive bugs.  Changed project images.  Added technologies used in projects

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
 
 let skillIcons = document.querySelectorAll('.skill-icon');
+let skillIcons2 = document.querySelectorAll('.skill-icon2');
 // console.log(skillIcons);
 
 // window.onresize = function() {
@@ -12,9 +13,19 @@ function resizeSkillIcons() {
             element.classList.add('skill-icon-small');
             // console.log('big window')
         })
+        skillIcons2.forEach( (element) => {
+            // element.classList.remove('fa-3x');
+            element.classList.add('skill-icon2-small');
+            // console.log('big window')
+        })
     } else {
         skillIcons.forEach( (element) => {
             element.classList.remove('skill-icon-small');
+            // element.classList.add('fa-3x');
+            // console.log('small window')
+        })
+        skillIcons2.forEach( (element) => {
+            element.classList.remove('skill-icon2-small');
             // element.classList.add('fa-3x');
             // console.log('small window')
         })
